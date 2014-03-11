372ec69ba2f24f174a71b580685caed92e68f558
pranav <pranav@pranavpc.com>
2014-03-11 16:34:19 +0800

    Edited the invalid redirect to make it work

--- dashboard.php
+++ dashboard.php
@@ -1,7 +1,10 @@
-<form action='src/webmail.php?right_frame=dashboard_helper.php' method='post' name='frm'>
-<input type='hidden' name='redirect_url' value='../plugins/dashboard_plugins/dashboard_plugins.php'>
+<form action='src/dashboard_helper.php' method='post' name='helper_form'>
+<?php
+$encode_url= urlencode("/plugins/dashboard_plugins/dashboard_plugins.php");
+?>
+<input type='hidden' name='redirect_url' value="<?php echo $encode_url; ?>">
 </form>
 <script language="JavaScript">
-document.frm.submit();
+document.helper_form.submit(); 
 </script>
 
--- src/dashboard_helper.php
+++ src/dashboard_helper.php
@@ -1,9 +1,12 @@
 <?php
-var_dump($_POST);
-echo <<<EOD
-<script>
 
-</script>
-EOD;
-/*$_REQUEST['redirect_url']*/
+if(!defined('SM_PATH'))
+    define('SM_PATH','../');
+
+include_once(SM_PATH . 'include/validate.php');
+
+session_start();
+$_SESSION['dashboard_url_redirect'] = $_REQUEST['redirect_url'];
+header("Location:webmail.php?right_frame=dashboard_loader.php");
+
 ?>
